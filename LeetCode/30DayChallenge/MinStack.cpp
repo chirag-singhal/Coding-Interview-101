4c7e1a2235583d7ff1d8e1c1f8f756c5dbdde0ce
chirag-singhal <csinghal208@gmail.com>
2020-04-10 18:17:57 +0530

    Day 10

---
+++ LeetCode/30DayChallenge/MinStack.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> s;
+    stack<int> min;
+    int size;
+    
+    MinStack() {
+        size = 0;    
+    }
+    
+    void push(int x) {
+        s.push(x);
+        if(min.empty())
+            min.push(x);
+        else if(min.top() >= x)
+            min.push(x);
+        size++;
+    }
+    
+    void pop() {
+        if(s.empty())
+            return;
+        if(min.top() == s.top())
+            min.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
