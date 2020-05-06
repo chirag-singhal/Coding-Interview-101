ce851fff8d455894651d0e8cd4b7194009a0b287
chirag-singhal <csinghal208@gmail.com>
2020-05-06 22:19:23 +0530

    Excel column title

---
+++ InterviewBit/Maths/ExcelColumnTitle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+std::string convertToTitle(int A) {
+    std::string s;
+    while(A > 0) {
+        int r2 = A % 26;
+        char c;
+        if(r2 == 0) {
+            c = 'Z';
+            A = A / 26 - 1;
+        }
+        else { 
+            c = 'A' + r2 - 1;
+            A = A / 26;
+        }
+        string s1(1, c);
+        s = s1 + s;
+    }
+    return s;
+}
