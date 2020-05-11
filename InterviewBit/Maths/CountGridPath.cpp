e198cbc4afd9104289317b76c27b912d94975bbc
chirag-singhal <csinghal208@gmail.com>
2020-05-11 22:19:54 +0530

    Grid Unique Paths

---
+++ InterviewBit/Maths/CountGridPath.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int uniquePaths(int A, int B) {
+    std::vector<int> v(B, 0);
+    vector<vector<int>> dp(A, v);
+    for(int i = 0; i < A; i++)
+        dp[i][0] = 1;
+    for(int i = 0; i < B; i++)
+        dp[0][i] = 1;
+    for(int i = 1; i < A; i++)
+        for(int j = 1; j < B; j++)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            
+    return dp[A - 1][B - 1];
+}
